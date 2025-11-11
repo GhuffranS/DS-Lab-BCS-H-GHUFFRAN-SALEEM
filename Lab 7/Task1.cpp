ffcc52704a77d127dc7d3dd134cafd8fa4c08b0d
GhuffranS <ghuffransaleem19@gmail.com>
2025-11-11 23:03:54 +0500

    Task1.cpp
    This program implements a family ancestry tree using a binary tree structure. It includes functions to calculate the height of the tree, print the levels of each member, and display leaf nodes.

---
+++ Lab 7/Task1.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+struct PersonNode {
+    string name;
+    PersonNode* mother;
+    PersonNode* father;
+
+    PersonNode(string n) {
+        name = n;
+        mother = NULL;
+        father = NULL;
+    }
+};
+
+int getHeight(PersonNode* node) {
+    if (node == NULL) {
+        return -1;
+    }
+    int leftHeight = getHeight(node->mother);
+    int rightHeight = getHeight(node->father);
+    
+     if (leftHeight > rightHeight) {
+        return 1 + leftHeight;
+    } else {
+        return 1 + rightHeight;
+    }
+}
+
+void printLeafNodes(PersonNode* node) {
+    if (node == NULL) {
+        return;
+    }
+    if (node->mother == NULL && node->father == NULL) {
+        cout << node->name << " " << endl;
+    }
+    printLeafNodes(node->mother);
+    printLeafNodes(node->father);
+}
+
+void printLevels(PersonNode* node, int level) {
+    if (node == NULL) {
+        return;
+    }
+    cout << node->name << " is at level " << level << endl;
+    printLevels(node->mother, level + 1);
+    printLevels(node->father, level + 1);
+}
+
+int main() {
+    cout << "--- Task 1: Family Ancestry Tree ---" << endl;
+
+    PersonNode* root = new PersonNode("Me");
+    root->mother = new PersonNode("Mom");
+    root->father = new PersonNode("Dad");
+
+    root->mother->mother = new PersonNode("Maternal GM");
+    root->mother->father = new PersonNode("Maternal GF");
+
+    root->father->mother = new PersonNode("Paternal GM");
+
+    cout << "Root: " << root->name << endl;
+    
+    cout << "\nHeight of the tree: " << getHeight(root) << endl;
+
+    cout << "\nLevel of each member:" << endl;
+    printLevels(root, 0);
+
+    cout << "\nLeaf nodes (Ancestors):" << endl;
+    printLeafNodes(root);
+
+    return 0;
+}
